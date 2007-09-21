86d7ef5a2683ff3fa6fa3cd8e05a4ecb853c2ed3
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2007-09-21 16:26:20 +0000

    Get hostname from configured certificate
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@29 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/web_send/src/org/web_send/graph/Host.java
+++ waterken/web_send/src/org/web_send/graph/Host.java
@@ -13,9 +13,11 @@ Host {
     /**
      * Creates a new model.
      * <p>
-     * The <code>factory</code> class must have a method with signature:
-     * <code>static public T build({@link Framework} framework)</code>
-     * </p>
+     * The factory class specified by the given<code>typename</code> must have
+     * a method with signature:</p>
+     * <p><code>static public T build({@link Framework} framework)</code></p>
+     * <p>or, a public constructor with signature:</p>
+     * <p><code>T({@link Eventual} _)</code></p>
      * @param <T> exported object type
      * @param label     model label
      * @param typename  object factory typename
