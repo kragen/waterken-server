e414c9b72a21cfe57184c4328cf1e2dd9c81f197
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2007-10-14 14:18:32 +0000

    more unit testing
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@94 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/example/src/org/waterken/eq/Main.java
+++ waterken/example/src/org/waterken/eq/Main.java
@@ -3,15 +3,20 @@
 package org.waterken.eq;
 
 import static org.ref_send.promise.Fulfilled.ref;
+import static org.ref_send.promise.eventual.Eventual.near;
 import static org.ref_send.test.Logic.and;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
+import org.joe_e.Equatable;
 import org.joe_e.Struct;
 import org.joe_e.Token;
 import org.ref_send.list.List;
+import org.ref_send.promise.Fulfilled;
 import org.ref_send.promise.NaN;
+import org.ref_send.promise.NegativeInfinity;
+import org.ref_send.promise.PositiveInfinity;
 import org.ref_send.promise.Promise;
 import org.ref_send.promise.eventual.Do;
 import org.ref_send.promise.eventual.Eventual;
@@ -68,16 +73,186 @@ Main extends Struct implements Test, Serializable {
     public Promise<Boolean>
     start() throws Exception {
         final ArrayList<Promise<Boolean>> r = new ArrayList<Promise<Boolean>>();
-        
-        // check double NaN handling
-        final Promise<Double> NaN = ref(Double.NaN);
-        check(NaN.equals(NaN));
-        check(!NaN.equals(ref(Double.NaN)));
+        r.add(testNormal());
+        r.add(testNull());
+        r.add(testDouble());
+        r.add(testFloat());
+        return and(_, r.toArray(new Promise[r.size()]));
+    }
+    
+    // org.waterken.eq.Main interface
+
+    /**
+     * Tests promises for a normal reference.
+     */
+    public Promise<Boolean>
+    testNormal() throws Exception {
+        final ArrayList<Promise<Boolean>> r = new ArrayList<Promise<Boolean>>();
+        class Normal implements Runnable, Equatable {
+            public void run() {}
+        }
+        final Normal x = new Normal();
+        final Runnable ix = x;
+        final Promise<Runnable> p = ref(ix);
+        check(p.equals(p));
+        check(ref(x).equals(p));
+        check(x == p.cast());
+        r.add(_.when(p, new Do<Runnable,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Runnable arg) throws Exception {
+                check(x == arg);
+                return ref(true);
+            }
+        }));
+        r.add(_.when(ix, new Do<Runnable,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Runnable arg) throws Exception {
+                check(x == arg);
+                return ref(true);
+            }
+        }));
+        final Runnable x_ = _._(ix);
+        check(x_.equals(x_));
+        check(_._(x_).equals(x_));
+        check(_._(ix).equals(x_));
+        check(_.cast(Runnable.class, p).equals(x_));
+        check(Eventual.promised(x_).equals(p));
+        check(x == near(x_));
+        r.add(_.when(x_, new Do<Runnable,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Runnable arg) throws Exception {
+                check(x == arg);
+                return ref(true);
+            }
+        }));
+        return and(_, r.toArray(new Promise[r.size()]));
+    }
+
+    /**
+     * Tests promises for a <code>null</code>.
+     */
+    public Promise<Boolean>
+    testNull() throws Exception {
+        final ArrayList<Promise<Boolean>> r = new ArrayList<Promise<Boolean>>();
+        final Promise<Runnable> p = ref(null);
+        check(p.equals(p));
+        check(!ref(null).equals(p));
         try {
-            NaN.cast();
+            p.cast();
+            check(false);
+        } catch (final NullPointerException e) {}
+        r.add(_.when(p, new Do<Runnable,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Runnable arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NullPointerException) {return ref(true);}
+                throw reason;
+            }
+        }));
+        r.add(_.when(null, new Do<Runnable,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Runnable arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NullPointerException) {return ref(true);}
+                throw reason;
+            }
+        }));
+        final Promise<Runnable> sneaky = Fulfilled.detach(null); 
+        r.add(_.when(sneaky, new Do<Runnable,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Runnable arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NullPointerException) {return ref(true);}
+                throw reason;
+            }
+        }));
+        final Runnable x_ = _.cast(Runnable.class, p);
+        check(x_.equals(x_));
+        check(_._(x_).equals(x_));
+        check(_.cast(Runnable.class, p).equals(x_));
+        check(Eventual.promised(x_).equals(p));
+        r.add(_.when(x_, new Do<Runnable,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Runnable arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NullPointerException) {return ref(true);}
+                throw reason;
+            }
+        }));
+        return and(_, r.toArray(new Promise[r.size()]));
+    }
+
+    /**
+     * Tests promises for {@link Double}.
+     */
+    public Promise<Boolean>
+    testDouble() throws Exception {
+        final ArrayList<Promise<Boolean>> r = new ArrayList<Promise<Boolean>>();
+        
+        // check normal handling
+        final Promise<Double> pMin = ref(Double.MIN_VALUE);
+        check(pMin.equals(pMin));
+        check(ref(Double.MIN_VALUE).equals(pMin));
+        check(Double.MIN_VALUE == pMin.cast());
+        r.add(_.when(pMin, new Do<Double,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                check(Double.MIN_VALUE == arg);
+                return ref(true);
+            }
+        }));
+        r.add(_.when(Double.MIN_VALUE, new Do<Double,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                check(Double.MIN_VALUE == arg);
+                return ref(true);
+            }
+        }));
+        
+        // check NaN handling
+        final Promise<Double> pNaN = ref(Double.NaN);
+        check(pNaN.equals(pNaN));
+        check(!pNaN.equals(ref(Double.NaN)));
+        try {
+            pNaN.cast();
             check(false);
         } catch (final NaN e) {}
-        r.add(_.when(NaN, new Do<Double,Promise<Boolean>>() {
+        r.add(_.when(pNaN, new Do<Double,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NaN) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Double.NaN, new Do<Double,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NaN) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Fulfilled.detach(Double.NaN),
+                     new Do<Double,Promise<Boolean>>() {
             public Promise<Boolean>
             fulfill(final Double arg) throws Exception {
                 throw new Exception();
@@ -89,6 +264,251 @@ Main extends Struct implements Test, Serializable {
             }
         }));
         
+        // check -infinity handling
+        final Promise<Double> pMinusInfinity = ref(Double.NEGATIVE_INFINITY);
+        check(pMinusInfinity.equals(pMinusInfinity));
+        check(!pMinusInfinity.equals(ref(Double.NEGATIVE_INFINITY)));
+        try {
+            pMinusInfinity.cast();
+            check(false);
+        } catch (final NegativeInfinity e) {}
+        r.add(_.when(pMinusInfinity, new Do<Double,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NegativeInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Double.NEGATIVE_INFINITY,new Do<Double,Promise<Boolean>>(){
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NegativeInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Fulfilled.detach(Double.NEGATIVE_INFINITY),
+                     new Do<Double,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NegativeInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        
+        // check +infinity handling
+        final Promise<Double> pPlusInfinity = ref(Double.POSITIVE_INFINITY);
+        check(pPlusInfinity.equals(pPlusInfinity));
+        check(!pPlusInfinity.equals(ref(Double.POSITIVE_INFINITY)));
+        try {
+            pPlusInfinity.cast();
+            check(false);
+        } catch (final PositiveInfinity e) {}
+        r.add(_.when(pPlusInfinity, new Do<Double,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof PositiveInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Double.POSITIVE_INFINITY,new Do<Double,Promise<Boolean>>(){
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof PositiveInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Fulfilled.detach(Double.POSITIVE_INFINITY),
+                     new Do<Double,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Double arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof PositiveInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+
+        return and(_, r.toArray(new Promise[r.size()]));
+    }
+
+    /**
+     * Tests promises for {@link Float}.
+     */
+    public Promise<Boolean>
+    testFloat() throws Exception {
+        final ArrayList<Promise<Boolean>> r = new ArrayList<Promise<Boolean>>();
+        
+        // check normal handling
+        final Promise<Float> pMin = ref(Float.MIN_VALUE);
+        check(pMin.equals(pMin));
+        check(ref(Float.MIN_VALUE).equals(pMin));
+        check(Float.MIN_VALUE == pMin.cast());
+        r.add(_.when(pMin, new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                check(Float.MIN_VALUE == arg);
+                return ref(true);
+            }
+        }));
+        r.add(_.when(Float.MIN_VALUE, new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                check(Float.MIN_VALUE == arg);
+                return ref(true);
+            }
+        }));
+        
+        // check NaN handling
+        final Promise<Float> pNaN = ref(Float.NaN);
+        check(pNaN.equals(pNaN));
+        check(!pNaN.equals(ref(Float.NaN)));
+        try {
+            pNaN.cast();
+            check(false);
+        } catch (final NaN e) {}
+        r.add(_.when(pNaN, new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NaN) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Float.NaN, new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NaN) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Fulfilled.detach(Float.NaN),
+                     new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NaN) { return ref(true); }
+                throw reason;
+            }
+        }));
+        
+        // check -infinity handling
+        final Promise<Float> pMinusInfinity = ref(Float.NEGATIVE_INFINITY);
+        check(pMinusInfinity.equals(pMinusInfinity));
+        check(!pMinusInfinity.equals(ref(Float.NEGATIVE_INFINITY)));
+        try {
+            pMinusInfinity.cast();
+            check(false);
+        } catch (final NegativeInfinity e) {}
+        r.add(_.when(pMinusInfinity, new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NegativeInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Float.NEGATIVE_INFINITY, new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NegativeInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Fulfilled.detach(Float.NEGATIVE_INFINITY),
+                     new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof NegativeInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        
+        // check +infinity handling
+        final Promise<Float> pPlusInfinity = ref(Float.POSITIVE_INFINITY);
+        check(pPlusInfinity.equals(pPlusInfinity));
+        check(!pPlusInfinity.equals(ref(Float.POSITIVE_INFINITY)));
+        try {
+            pPlusInfinity.cast();
+            check(false);
+        } catch (final PositiveInfinity e) {}
+        r.add(_.when(pPlusInfinity, new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof PositiveInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Float.POSITIVE_INFINITY, new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof PositiveInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+        r.add(_.when(Fulfilled.detach(Float.POSITIVE_INFINITY),
+                     new Do<Float,Promise<Boolean>>() {
+            public Promise<Boolean>
+            fulfill(final Float arg) throws Exception {
+                throw new Exception();
+            }
+            public Promise<Boolean>
+            reject(final Exception reason) throws Exception {
+                if (reason instanceof PositiveInfinity) { return ref(true); }
+                throw reason;
+            }
+        }));
+
         return and(_, r.toArray(new Promise[r.size()]));
     }
     
