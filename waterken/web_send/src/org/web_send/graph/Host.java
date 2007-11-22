3359e1af1a387853fb9e051b674b9c9471e964cd
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2007-11-22 01:28:21 +0000

    new API to spawn a model without specifying a label
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@187 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/web_send/src/org/web_send/graph/Host.java
+++ waterken/web_send/src/org/web_send/graph/Host.java
@@ -7,15 +7,25 @@ package org.web_send.graph;
  */
 public interface
 Host {
+    
+    /**
+     * Creates a model.
+     * <p>
+     * The <code>factory</code> MUST have a method with signature:</p>
+     * <p><code>static public T build({@link Framework} framework)</code>
+     * </p>
+     * @param <T> exported object type
+     * @param factory object factory
+     * @return promise for an object exported from the new model
+     */
+    <T> T
+    spawn(Class<?> factory);
 
     /**
-     * Creates a new model.
-     * <p>
-     * The <code>factory</code> must have a method with signature:</p>
-     * <p><code>static public T build({@link Framework} framework)</code></p>
+     * Creates a model with a specific label.
      * @param <T> exported object type
      * @param label     model label
-     * @param factory   object factory typename
+     * @param factory   object factory, same requirements as in {@link #spawn}     * @param factory   object factory
      * @return promise for an object exported from the new model
      * @throws Collision    <code>label</code> has already been used
      */
