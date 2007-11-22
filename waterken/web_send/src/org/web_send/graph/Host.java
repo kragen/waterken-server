ac7131ff12f2b9ac5ad7a079ab7d75fd83db55e6
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2007-11-22 00:50:30 +0000

    better syntax for model creation
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@183 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/web_send/src/org/web_send/graph/Host.java
+++ waterken/web_send/src/org/web_send/graph/Host.java
@@ -2,9 +2,6 @@
 // found at http://www.opensource.org/licenses/mit-license.html
 package org.web_send.graph;
 
-import org.ref_send.promise.Promise;
-import org.ref_send.promise.eventual.Eventual;
-
 /**
  * A model maker.
  */
@@ -16,14 +13,12 @@ Host {
      * <p>
      * The <code>factory</code> must have a method with signature:</p>
      * <p><code>static public T build({@link Framework} framework)</code></p>
-     * <p>or, a public constructor with signature:</p>
-     * <p><code>T({@link Eventual} _)</code></p>
      * @param <T> exported object type
      * @param label     model label
      * @param factory   object factory typename
      * @return promise for an object exported from the new model
      * @throws Collision    <code>label</code> has already been used
      */
-    <T> Promise<T>
+    <T> T
     claim(String label, Class<?> factory) throws Collision;
 }
