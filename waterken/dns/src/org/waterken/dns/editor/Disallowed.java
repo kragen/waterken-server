9c03aa72c53e848b8e53e4c8f7002ba6d3af41fc
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2007-12-02 21:01:02 +0000

    fixing Javadoc bug
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@231 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/dns/src/org/waterken/dns/editor/Disallowed.java
+++ waterken/dns/src/org/waterken/dns/editor/Disallowed.java
@@ -1,9 +1,9 @@
 // Copyright 2007 Waterken Inc. under the terms of the MIT X license
 // found at http://www.opensource.org/licenses/mit-license.html
-
 package org.waterken.dns.editor;
 
 import org.ref_send.deserializer;
+import org.waterken.dns.Resource;
 
 /**
  * A disallowed {@link Resource}.
