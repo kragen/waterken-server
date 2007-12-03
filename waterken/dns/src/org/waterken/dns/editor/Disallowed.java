8c1ae67e835894e00078f4785ae212c744195032
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2007-12-03 17:57:18 +0000

    Joe-E fix
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@240 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/dns/src/org/waterken/dns/editor/Disallowed.java
+++ waterken/dns/src/org/waterken/dns/editor/Disallowed.java
@@ -2,6 +2,7 @@
 // found at http://www.opensource.org/licenses/mit-license.html
 package org.waterken.dns.editor;
 
+import org.joe_e.Powerless;
 import org.ref_send.deserializer;
 import org.waterken.dns.Resource;
 
@@ -9,7 +10,7 @@ import org.waterken.dns.Resource;
  * A disallowed {@link Resource}.
  */
 public class
-Disallowed extends RuntimeException {
+Disallowed extends RuntimeException implements Powerless {
     static private final long serialVersionUID = 1L;
 
     /**
