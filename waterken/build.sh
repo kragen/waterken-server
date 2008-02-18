32a35289fd1c40dc1eb8bb87cd3669b8166c1773
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2008-02-18 21:19:49 +0000

    using the vat term consistently
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@447 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/build.sh
+++ waterken/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p db
-touch db/.persistent-object-store
+mkdir -p vat
+touch vat/.persistent-object-store
 (cd joe-e/; ./build.sh $@)
 (cd ref_send/; ./build.sh $@)
 (cd web_send/; ./build.sh $@)
