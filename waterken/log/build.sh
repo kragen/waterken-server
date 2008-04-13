26d5586fd6ad096ac7b359955d5c5a2ee81703f2
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2008-04-13 22:16:59 +0000

    fixing build path
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@559 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/log/build.sh
+++ waterken/log/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if [ "$OS" = 'Windows_NT' ]
 then
-    CLASSPATH='..\joe-e\bin;..\ref_send\bin;..\web_send\bin;..\shared\bin'
+    CLASSPATH='..\joe-e\bin;..\ref_send\bin;..\web_send\bin'
 else
-    CLASSPATH='../joe-e/bin:../ref_send/bin:../web_send/bin:../shared/bin'
+    CLASSPATH='../joe-e/bin:../ref_send/bin:../web_send/bin'
 fi
 
 echo 'Building log project...'
