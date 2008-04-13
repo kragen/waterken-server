511dca1062834b8c265a095f5032084f26256d61
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2008-04-13 20:20:46 +0000

    adding shared to build path
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@547 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/build.sh
+++ waterken/build.sh
@@ -5,6 +5,7 @@ touch vat/.persistent-object-store
 (cd joe-e/; ./build.sh $@)
 (cd ref_send/; ./build.sh $@)
 (cd web_send/; ./build.sh $@)
+(cd shared/; ./build.sh $@)
 (cd example/; ./build.sh $@)
 (cd network/; ./build.sh $@)
 (cd persistence/; ./build.sh $@)
