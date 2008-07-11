925248788e346247c7f8ee14c8deef8e43a82160
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2008-07-11 23:53:34 +0000

    refactoring JSON serialization
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@679 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/genkey/src/org/waterken/genkey/GenKey.java
+++ waterken/genkey/src/org/waterken/genkey/GenKey.java
@@ -210,7 +210,7 @@ GenKey {
            public void
            run() throws Exception {
                final Redirectory redirectory_ = (Redirectory)Config.browser.
-                   connect.run(Redirectory.class, redirectoryURL);
+                   connect.run(Redirectory.class, redirectoryURL, null);
                _.when(redirectory_.register(fingerprint),
                       new Do<DomainMaster,Void>() {
                    public Void
