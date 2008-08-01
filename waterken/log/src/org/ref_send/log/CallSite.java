f91cbfe80daaf5b6685cec500cd3d0c7895e400c
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2008-08-01 23:05:44 +0000

    better documentation
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@729 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/log/src/org/ref_send/log/CallSite.java
+++ waterken/log/src/org/ref_send/log/CallSite.java
@@ -32,22 +32,38 @@ CallSite extends Struct implements Powerless, Record, Serializable {
     /**
      * call site's position within the {@linkplain #source} (optional)
      * <p>
-     * The expected structure of this table defines a span from the start of
-     * the relevant source code to the end. The first row in the table is the
-     * start of the span and the second row is the end of the span. Each row
-     * lists the line number followed by the column number. For example, a span
-     * of code starting on line 5, column 8 and ending on line 6, column 12 is
-     * encoded as:
+     * The expected structure of this table defines a span from the start of the
+     * relevant source code to the end. The first row in the table is the start
+     * of the span and the second row is the end of the span. Each row lists the
+     * line number followed by the column number. For example, a span of code
+     * starting on line 5, column 8 and ending on line 6, column 12 is encoded
+     * as:
      * </p>
-     * <p><code>[ [ 5, 8 ], [ 6, 12 ] ]</code></p>
+     * <p>
+     * <code>[ [ 5, 8 ], [ 6, 12 ] ]</code>
+     * </p>
+     * <p>
+     * The delimited span is inclusive, meaning the character at line 6, column
+     * 12 is included in the span defined above.
+     * </p>
      * <p>
      * If the end of the span is unknown, it may be omitted. If the column
      * number is unknown, it may also be omitted. For example, in the case where
      * only the starting line number is known:
      * </p>
-     * <p><code>[ [ 5 ] ]</code></p>
+     * <p>
+     * <code>[ [ 5 ] ]</code>
+     * </p>
      * <p>
      * If source span information is unknown, this member is <code>null</code>.
+     * </p>
+     * <p>
+     * Both lines and columns are numbered starting from one, so the first
+     * character in a source file is at <code>[ 1, 1 ]</code>. A column is a
+     * <em>Unicode code unit</em>, the same unit represented by a Java
+     * <code>char</code>. Lines are separated by any character sequence
+     * considered a Unicode <a
+     * href="http://en.wikipedia.org/wiki/Newline#Unicode">line terminator</a>.
      * </p>
      */
     public final PowerlessArray<IntArray> span;
