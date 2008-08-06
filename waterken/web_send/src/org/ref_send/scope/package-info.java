2f59c169e3d41be84785219f388330625d442741
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2008-08-06 01:13:17 +0000

    put JSON serialization in its own project
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@737 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/web_send/src/org/ref_send/scope/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2007-2008 Waterken Inc. under the terms of the MIT X license
-// found at http://www.opensource.org/licenses/mit-license.html
-
-/**
- * A [ name =&gt; value ] record.
- */
-@org.joe_e.IsJoeE package org.ref_send.scope;
