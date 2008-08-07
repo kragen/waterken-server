33d354c457aa677c912812169228b8f73ebde213
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2008-08-07 21:01:43 +0000

    update date
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@778 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/syntax/src/org/waterken/syntax/json/JSONSerializer.java
+++ waterken/syntax/src/org/waterken/syntax/json/JSONSerializer.java
@@ -1,4 +1,4 @@
-// Copyright 2007 Waterken Inc. under the terms of the MIT X license
+// Copyright 2007-2008 Waterken Inc. under the terms of the MIT X license
 // found at http://www.opensource.org/licenses/mit-license.html
 package org.waterken.syntax.json;
 
