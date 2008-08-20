b4975430d31cfc98b36f76d7919f287e0a8d7ce1
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2008-08-20 10:22:54 +0000

    special log entry for an uncaught exception
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@857 ec340d7e-3b38-0410-a49c-f1b72a8843da

---
+++ waterken/log/src/org/ref_send/log/Problem.java
@@ -0,0 +1,35 @@
+// Copyright 2008 Waterken Inc. under the terms of the MIT X license
+// found at http://www.opensource.org/licenses/mit-license.html
+package org.ref_send.log;
+
+import org.ref_send.deserializer;
+import org.ref_send.name;
+
+/**
+ * Logs an uncaught exception.
+ */
+public class
+Problem extends Comment {
+    static private final long serialVersionUID = 1L;
+
+    /**
+     * uncaught exception
+     */
+    public final Exception reason;
+    
+    /**
+     * Constructs an instance.
+     * @param anchor    {@link #anchor}
+     * @param trace     {@link #trace}
+     * @param text      {@link #text}
+     * @param reason    {@link #reason}
+     */
+    public @deserializer
+    Problem(@name("anchor") final Anchor anchor,
+            @name("trace") final Trace trace,
+            @name("text") final String text,
+            @name("reason") final Exception reason) {
+        super(anchor, trace, text);
+        this.reason = reason;
+    }
+}
