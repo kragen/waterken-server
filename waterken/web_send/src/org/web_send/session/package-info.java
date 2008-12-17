4e4b1d0ea23124717c72ae3d6f5dc58d6321931f
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2008-12-17 02:17:55 +0000

    in progress changes for next version of server
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@920 ec340d7e-3b38-0410-a49c-f1b72a8843da

---
+++ waterken/web_send/src/org/web_send/session/package-info.java
@@ -0,0 +1,7 @@
+// Copyright 2007-2008 Waterken Inc. under the terms of the MIT X license
+// found at http://www.opensource.org/licenses/mit-license.html
+
+/**
+ * Remote messaging structures used by the implementation layer.
+ */
+@org.joe_e.IsJoeE package org.web_send.session;
