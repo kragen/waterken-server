5b51c39fbd45fa5be914ec849e4b18f719730ed1
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-01-23 15:52:32 +0000

    in progress changes
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@974 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/web_send/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo 'Cleaning web_send project...'
-rm -rf bin/
--- waterken/web_send/src/org/web_send/graph/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2007-2008 Waterken Inc. under the terms of the MIT X license
-// found at http://www.opensource.org/licenses/mit-license.html
-
-/**
- * A reference graph interface.
- */
-@org.joe_e.IsJoeE package org.web_send.graph;
