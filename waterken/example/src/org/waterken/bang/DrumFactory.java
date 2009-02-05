b50174990aa31bf9b723c5f622aae8e0b303455c
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-02-05 01:09:13 +0000

    in progress changes for next version of server
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1023 ec340d7e-3b38-0410-a49c-f1b72a8843da

---
+++ waterken/example/src/org/waterken/bang/DrumFactory.java
@@ -0,0 +1,27 @@
+// Copyright 2009 Waterken Inc. under the terms of the MIT X license
+// found at http://www.opensource.org/licenses/mit-license.html
+package org.waterken.bang;
+
+import static org.ref_send.promise.Fulfilled.ref;
+
+import java.io.Serializable;
+
+import org.joe_e.Struct;
+import org.ref_send.promise.Promise;
+
+/**
+ * 
+ */
+public final class
+DrumFactory extends Struct implements Serializable {
+    static private final long serialVersionUID = 1L;
+    
+    private
+    DrumFactory() {}
+
+    static public Promise<DrumFactory>
+    make() { return ref(new DrumFactory()); }
+    
+    public Drum
+    makeDrum() { return Bang.make(); }
+}
