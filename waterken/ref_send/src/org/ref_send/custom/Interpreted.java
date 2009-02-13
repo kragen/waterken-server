6700c7ce8945f4953af9eb4916dc378d2237e7a5
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-02-13 23:57:57 +0000

    skip interpreted stuff for now
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1110 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/ref_send/src/org/ref_send/custom/Interpreted.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2009 Waterken Inc. under the terms of the MIT X license
-// found at http://www.opensource.org/licenses/mit-license.html
-package org.ref_send.custom;
-
-/**
- * An object that does its own method dispatch.
- */
-public interface Interpreted {}
--- waterken/ref_send/src/org/ref_send/custom/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2008 Waterken Inc. under the terms of the MIT X license
-// found at http://www.opensource.org/licenses/mit-license.html
-
-/**
- * Interface to an object that does its own method dispatch. 
- */
-@org.joe_e.IsJoeE package org.ref_send.custom;
