6eac629f545d82ab79a39f219e56e739512cecc7
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-02-14 00:08:46 +0000

    skip interpreted stuff for now
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1111 ec340d7e-3b38-0410-a49c-f1b72a8843da

---
+++ waterken/syntax/src/org/ref_send/scope/Scope.java
@@ -0,0 +1,73 @@
+// Copyright 2008 Waterken Inc. under the terms of the MIT X license
+// found at http://www.opensource.org/licenses/mit-license.html
+package org.ref_send.scope;
+
+import java.io.Serializable;
+
+import org.joe_e.Selfless;
+import org.joe_e.array.ConstArray;
+
+/**
+ * A [ name =&gt; value ] record.
+ */
+public final class
+Scope<T> implements Selfless, Serializable {
+    static private final long serialVersionUID = 1L;
+    
+    /**
+     * metadata
+     */
+    public final Layout meta;
+
+    /**
+     * each corresponding value
+     */
+    public final ConstArray<? extends T> values;
+    
+    /**
+     * Constructs an instance.
+     * @param meta      {@link #meta}
+     * @param values    {@link #values}
+     */
+    public
+    Scope(final Layout meta, final ConstArray<? extends T> values) {
+        if (meta.names.length()!=values.length()){throw new RuntimeException();}
+        
+        this.meta = meta;
+        this.values = values;
+    }
+    
+    // java.lang.Object interface
+    
+    /**
+     * Is the given object the same?
+     * @param o compared to object
+     * @return <code>true</code> if the same, else <code>false</code>
+     */
+    public boolean
+    equals(final Object o) {
+        return null != o && Scope.class == o.getClass() &&
+               values.equals(((Scope<?>)o).values) &&
+               meta.equals(((Scope<?>)o).meta);
+    }
+    
+    /**
+     * Calculates the hash code.
+     */
+    public int
+    hashCode() { return 0x4EF5C09E + meta.hashCode() + values.hashCode(); }
+    
+    // org.ref_send.scope.Scope interface
+    
+    /**
+     * Gets the named value.
+     * @param <R>   expected type of value
+     * @param name  member name
+     * @return member value, or <code>null</code> if unspecified
+     */
+    public @SuppressWarnings("unchecked") <R extends T> R
+    get(final String name) {
+        final int i = meta.find(name);
+        return -1 != i ? (R)values.get(i) : null;
+    }
+}
