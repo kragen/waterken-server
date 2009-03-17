3d469cdd33cdde5d0757970f8cdd766297c9fed1
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-03-17 18:42:06 +0000

    DOS batch files for building
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1250 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/dns/build.bat
+++ waterken/dns/build.bat
@@ -3,20 +3,7 @@ echo 'Building dns project...'
 mkdir bin
 javac -d bin ^
   -classpath "..\joe-e\bin;..\ref_send\bin;..\network\bin;..\log\bin;..\persistence\bin;..\shared\bin" ^
-  src\org\waterken\dns\editor\BadFormat.java ^
-  src\org\waterken\dns\editor\HostMaker.java ^
-  src\org\waterken\dns\editor\package-info.java ^
-  src\org\waterken\dns\editor\Registrar.java ^
-  src\org\waterken\dns\editor\RegistrarMaker.java ^
-  src\org\waterken\dns\editor\ResourceVariable.java ^
-  src\org\waterken\dns\editor\UnsupportedClass.java ^
-  src\org\waterken\dns\editor\UnsupportedResource.java ^
-  src\org\waterken\dns\editor\UnsupportedType.java ^
-  src\org\waterken\dns\editor\redirectory\package-info.java ^
-  src\org\waterken\dns\editor\redirectory\Redirectory.java ^
-  src\org\waterken\dns\editor\redirectory\RedirectoryMaker.java ^
-  src\org\waterken\dns\udp\NameServer.java ^
-  src\org\waterken\dns\udp\package-info.java ^
-  src\org\waterken\menu\Menu.java ^
-  src\org\waterken\menu\package-info.java ^
-  src\org\waterken\menu\TooMany.java
+  src\org\waterken\dns\editor\*.java ^
+  src\org\waterken\dns\editor\redirectory\*.java ^
+  src\org\waterken\dns\udp\*.java ^
+  src\org\waterken\menu\*.java
