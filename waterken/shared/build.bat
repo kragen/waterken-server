7f0b8d2d900a180e0c4810d05bba7d502db34c9c
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-03-17 18:43:40 +0000

    DOS batch files for building
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1253 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/shared/build.bat
+++ waterken/shared/build.bat
@@ -3,5 +3,4 @@ echo 'Building shared project...'
 mkdir bin
 javac -d bin ^
   -classpath "..\joe-e\bin;..\ref_send\bin" ^
-  src\org\ref_send\test\Logic.java ^
-  src\org\ref_send\test\package-info.java
+  src\org\ref_send\test\*.java
