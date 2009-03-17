d0e52819fa3a5b0f96f50da4fc0c76f2f6c4b7e2
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-03-17 18:41:19 +0000

    DOS batch files for building
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1248 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/example/build.bat
+++ waterken/example/build.bat
@@ -3,26 +3,9 @@ echo 'Building example project...'
 mkdir bin
 javac -d bin ^
   -classpath "..\joe-e\bin;..\ref_send\bin;..\shared\bin" ^
-  src\org\waterken\all\All.java ^
-  src\org\waterken\all\package-info.java ^
-  src\org\waterken\bang\Bang.java ^
-  src\org\waterken\bang\Beat.java ^
-  src\org\waterken\bang\Drum.java ^
-  src\org\waterken\bang\DrumFactory.java ^
-  src\org\waterken\bang\package-info.java ^
-  src\org\waterken\bounce\AllTypes.java ^
-  src\org\waterken\bounce\Bounce.java ^
-  src\org\waterken\bounce\package-info.java ^
-  src\org\waterken\bounce\Pitch.java ^
-  src\org\waterken\bounce\Wall.java ^
-  src\org\waterken\eq\package-info.java ^
-  src\org\waterken\eq\Sneaky.java ^
-  src\org\waterken\eq\SoundCheck.java ^
-  src\org\waterken\factorial\Factorial.java ^
-  src\org\waterken\factorial\FactorialN.java ^
-  src\org\waterken\factorial\package-info.java ^
-  src\org\waterken\serial\Element.java ^
-  src\org\waterken\serial\package-info.java ^
-  src\org\waterken\serial\PopPushN.java ^
-  src\org\waterken\serial\Serial.java ^
-  src\org\waterken\serial\Series.java
+  src\org\waterken\all\*.java ^
+  src\org\waterken\bang\*.java ^
+  src\org\waterken\bounce\*.java ^
+  src\org\waterken\eq\*.java ^
+  src\org\waterken\factorial\*.java ^
+  src\org\waterken\serial\*.java
