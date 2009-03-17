6ab3af7a7f2900c98d70e8ae57452d81e5348da1
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-03-17 18:44:56 +0000

    DOS batch files for building
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1256 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/ref_send/build.bat
+++ waterken/ref_send/build.bat
@@ -3,27 +3,7 @@ echo 'Building ref_send project...'
 mkdir bin
 javac -d bin ^
   -classpath "..\joe-e\bin" ^
-  src\org\ref_send\deserializer.java ^
-  src\org\ref_send\name.java ^
-  src\org\ref_send\package-info.java ^
-  src\org\ref_send\Record.java ^
-  src\org\ref_send\list\List.java ^
-  src\org\ref_send\list\package-info.java ^
-  src\org\ref_send\promise\Channel.java ^
-  src\org\ref_send\promise\Compose.java ^
-  src\org\ref_send\promise\Deferred.java ^
-  src\org\ref_send\promise\Detachable.java ^
-  src\org\ref_send\promise\Do.java ^
-  src\org\ref_send\promise\Eventual.java ^
-  src\org\ref_send\promise\Failure.java ^
-  src\org\ref_send\promise\Inline.java ^
-  src\org\ref_send\promise\Invoke.java ^
-  src\org\ref_send\promise\Log.java ^
-  src\org\ref_send\promise\package-info.java ^
-  src\org\ref_send\promise\Promise.java ^
-  src\org\ref_send\promise\Receiver.java ^
-  src\org\ref_send\promise\Rejected.java ^
-  src\org\ref_send\promise\Resolver.java ^
-  src\org\ref_send\promise\Vat.java ^
-  src\org\ref_send\type\package-info.java ^
-  src\org\ref_send\type\Typedef.java
+  src\org\ref_send\*.java ^
+  src\org\ref_send\list\*.java ^
+  src\org\ref_send\promise\*.java ^
+  src\org\ref_send\type\*.java
