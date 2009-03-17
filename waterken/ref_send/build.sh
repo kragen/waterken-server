3be7c99b86d7032f2368fba3e0147b8eabdee52f
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-03-17 17:42:55 +0000

    DOS batch files for building
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1242 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/ref_send/build.sh
+++ waterken/ref_send/build.sh
@@ -3,4 +3,4 @@
 echo 'Building ref_send project...'
 rm -rf bin/
 mkdir -p bin
-javac -classpath "../joe-e/bin" -d bin/ `find src/ -name '*.java'` $@
+echo javac -classpath "../joe-e/bin" -d bin/ `find src/ -name '*.java'` $@
