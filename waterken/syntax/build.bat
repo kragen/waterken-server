a7234455aa3edd9ac09236a596149aef99478522
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-03-17 18:42:38 +0000

    DOS batch files for building
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1251 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/syntax/build.bat
+++ waterken/syntax/build.bat
@@ -3,22 +3,7 @@ echo 'Building syntax project...'
 mkdir bin
 javac -d bin ^
   -classpath "..\joe-e\bin;..\ref_send\bin" ^
-  src\org\ref_send\scope\Layout.java ^
-  src\org\ref_send\scope\package-info.java ^
-  src\org\ref_send\scope\Scope.java ^
-  src\org\waterken\syntax\BadSyntax.java ^
-  src\org\waterken\syntax\Deserializer.java ^
-  src\org\waterken\syntax\Exporter.java ^
-  src\org\waterken\syntax\Importer.java ^
-  src\org\waterken\syntax\package-info.java ^
-  src\org\waterken\syntax\Serializer.java ^
-  src\org\waterken\syntax\Syntax.java ^
-  src\org\waterken\syntax\config\Config.java ^
-  src\org\waterken\syntax\config\package-info.java ^
-  src\org\waterken\syntax\json\Java.java ^
-  src\org\waterken\syntax\json\JSONDeserializer.java ^
-  src\org\waterken\syntax\json\JSONLexer.java ^
-  src\org\waterken\syntax\json\JSONParser.java ^
-  src\org\waterken\syntax\json\JSONSerializer.java ^
-  src\org\waterken\syntax\json\JSONWriter.java ^
-  src\org\waterken\syntax\json\package-info.java
+  src\org\ref_send\scope\*.java ^
+  src\org\waterken\syntax\*.java ^
+  src\org\waterken\syntax\config\*.java ^
+  src\org\waterken\syntax\json\*.java
