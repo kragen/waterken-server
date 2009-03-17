580e94079a0f69b6b5ff127d5ddc989ff03967f5
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-03-17 18:44:04 +0000

    DOS batch files for building
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1254 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/genkey/build.bat
+++ waterken/genkey/build.bat
@@ -4,7 +4,5 @@ CALL jars.bat
 mkdir bin
 javac -d bin ^
   -classpath "..\joe-e\bin;..\ref_send\bin;..\network\bin;..\log\bin;..\persistence\bin;..\syntax\bin;..\remote\bin;..\shared\bin;..\dns\bin" ^
-  src\org\waterken\genkey\GenKey.java ^
-  src\org\waterken\genkey\package-info.java ^
-  src\org\waterken\genkey\maker\Maker.java ^
-  src\org\waterken\genkey\maker\package-info.java
+  src\org\waterken\genkey\*.java ^
+  src\org\waterken\genkey\maker\*.java
