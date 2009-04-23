b9eef7feac3509df9792d9f0be3abbc1a81dd29b
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-04-23 18:25:15 +0000

    fixing build scripts
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1397 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/ref_send/build.sh
+++ waterken/ref_send/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+if [ "$OS" = 'Windows_NT' ]
+then
+    CLASSPATH='..\joe-e\bin'
+else
+    CLASSPATH='../joe-e/bin'
+fi
 
 echo 'Building ref_send project...'
 rm -rf bin/
 mkdir -p bin
-echo javac -classpath "../joe-e/bin" -d bin/ `find src/ -name '*.java'` $@
+javac -classpath $CLASSPATH -d bin/ `find src/ -name '*.java'` $@
