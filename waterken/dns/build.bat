d2c5fce1b50c194f1774d15c6a5a12d5f07ca515
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2009-06-24 17:06:59 +0000

    fixing DOS build scripts
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1630 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/dns/build.bat
+++ waterken/dns/build.bat
@@ -4,6 +4,5 @@ mkdir bin
 javac -d bin ^
   -classpath "..\joe-e\bin;..\ref_send\bin;..\network\bin;..\log\bin;..\persistence\bin;..\shared\bin" ^
   src\org\waterken\dns\editor\*.java ^
-  src\org\waterken\dns\editor\redirectory\*.java ^
   src\org\waterken\dns\udp\*.java ^
   src\org\waterken\menu\*.java
--- waterken/genkey/build.bat
+++ waterken/genkey/build.bat
@@ -4,5 +4,4 @@ CALL jars.bat
 mkdir bin
 javac -d bin ^
   -classpath "..\joe-e\bin;..\ref_send\bin;..\network\bin;..\log\bin;..\persistence\bin;..\syntax\bin;..\remote\bin;..\shared\bin;..\dns\bin" ^
-  src\org\waterken\genkey\*.java ^
-  src\org\waterken\genkey\maker\*.java
+  src\org\waterken\genkey\*.java
