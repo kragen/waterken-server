1af1f271ac310787b1d839f03a77de9749201a66
tjclose <tjclose@ec340d7e-3b38-0410-a49c-f1b72a8843da>
2010-08-21 01:39:50 +0000

    Java 1.6 and 1.5 disagree about use of unchecked. Favor 1.6
    git-svn-id: https://svn.code.sf.net/p/waterken/code/server/trunk@1789 ec340d7e-3b38-0410-a49c-f1b72a8843da

--- waterken/ref_send/src/org/ref_send/promise/Eventual.java
+++ waterken/ref_send/src/org/ref_send/promise/Eventual.java
@@ -672,9 +672,7 @@ Eventual implements Selfless, Serializable {
                 try {
                     if (trusted(value)) {
                         log.got(here + "#w" + message, null, null);
-                        final @SuppressWarnings("unchecked") Local<? extends T>
-                          promise = ((Local<? extends T>)value);
-                        promise.when(T, observer);
+                        ((Local<? extends T>)value).when(T, observer);
                     } else {
                         // AUDIT: call to untrusted application code
                         sample(value, observer, log, here + "#w" + message);
@@ -822,14 +820,11 @@ Eventual implements Selfless, Serializable {
         private void
         set(final Class<?> T, Promise<? extends T> p) {
             if (p instanceof Fulfilled<?>) {
-                final @SuppressWarnings("unchecked") Fulfilled<? extends T>
-                  fulfilled = (Fulfilled<? extends T>)p;
-                p = fulfilled.getState();
+                p = ((Fulfilled<? extends T>)p).getState();
                 log.fulfilled(here + "#p" + condition);
             } else if (p instanceof Rejected<?>) {
-                final @SuppressWarnings("unchecked") Rejected<? extends T>
-                  rejected = (Rejected<? extends T>)p;
-                log.rejected(here + "#p" + condition, rejected.reason);
+                log.rejected(here + "#p" + condition,
+                			 ((Rejected<? extends T>)p).reason);
             } else {
                 log.resolved(here + "#p" + condition);
             }
